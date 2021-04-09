da7003366358e687d81538bf03fe5b6c123b2b6c
LeonardoCg <leonardocg9492@gmail.com>
2021-04-08 22:32:10 -0500

    aside acabado no responsive

--- src/components/layout/aside/Aside.jsx
+++ src/components/layout/aside/Aside.jsx
@@ -1,10 +1,23 @@
 import React from 'react'
 import styles from './aside.module.scss'
+import Menu from '../../menu/Menu'
 
 const Aside = () => {
   return (
     <aside className={styles.navigation}>
-      Este es el aside
+      <h1 className={styles.storeName}>La Petite Obsession</h1>
+      <Menu/>
+      <div className={styles.socials}>
+        <a href='!#' className={`${styles.socialsLink} ${styles.facebook}`} >
+          <i class="fab fa-facebook-f"></i>
+        </a>
+        <a href='!#' className={`${styles.socialsLink} ${styles.instagram}`} >
+          <i class="fab fa-instagram"></i>
+        </a>
+        <a href='!#' className={`${styles.socialsLink} ${styles.whatsapp}`} >
+          <i class="fab fa-whatsapp"></i>
+        </a>
+      </div>
     </aside>
   )
 }
---
+++ src/components/menu/Menu.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import styles from './menu.module.scss'
+
+const Menu = () => {
+  return (
+    <nav className={styles.menu}>
+      <p className={styles.menuItem}>Pelucas</p>
+      <p className={styles.menuItem}>Accesorios</p>
+      <p className={styles.menuItem}>Lenceria</p>
+    </nav>
+  )
+}
+
+export default Menu
