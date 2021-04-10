eb49d8086af07c533fbe8ca55e9a3c5890a1c1db
LeonardoCg <leonardocg9492@gmail.com>
2021-04-09 19:27:36 -0500

    about y categorias responsive terminadas

--- src/components/pages/inicio/Inicio.jsx
+++ src/components/pages/inicio/Inicio.jsx
@@ -10,6 +10,30 @@ const Inicio = () => {
           <img src={require('../../../img/banner/banner.jpg').default} alt="banner"/>
         </div>
         <SectionTitle title='Bienvenido!'/>
+        <div className={styles.about}>
+          <figure className={styles.imagen}>
+            <img src={require('../../../img/logos/logo.jpg').default} alt="petite obsession logo"/>
+          </figure>
+          <div className={styles.text}>
+            <h3>¿A qué nos dedicamos?</h3>
+            <p>Somos una tienda online que se dedica a la venta de pelucas, accesorios, trajes y lenceria para que puedas realizar los atuendos que tu obsesion te demande.</p>
+          </div>
+        </div>
+        <SectionTitle title='Categorias'/>
+        <div className={styles.categorias}>
+          <figure className={styles.category}>
+            <img src={require('../../../img/categorias/pelucas.jpg').default} alt="pelucas"/>
+            <p>Pelucas</p>  
+          </figure>
+          <figure className={styles.category}>
+            <img src={require('../../../img/categorias/accesorios.jpg').default} alt="accesorios"/>
+            <p className={styles.accesorios}>Accesorios</p>  
+          </figure>
+          <figure className={styles.category}>
+            <img src={require('../../../img/categorias/lenceria.jpg').default} alt="lenceria"/>
+            <p>Lenceria</p>  
+          </figure>
+        </div>
       </section>
     </>
   )
