497d6c6a7abfac4957a78911ca1191662690286d
LeonardoCg <leonardocg9492@gmail.com>
2021-04-24 15:14:26 -0500

    usar link en vez de nav link

--- src/components/button/Button.jsx
+++ src/components/button/Button.jsx
@@ -1,13 +1,13 @@
 import React from 'react'
 import styles from './button.module.scss'
-import { NavLink } from 'react-router-dom'
+import { Link } from 'react-router-dom'
 
 const Button = ({text, color, click, route}) => {
   const {btn, btnWhite, btnPurple} = styles
   return (
-    <NavLink exact to={route} onClick={click} className={color ? [btn,btnPurple].join(' ') : [btn,btnWhite].join(' ')}>
+    <Link exact to={route} onClick={click} className={color ? [btn,btnPurple].join(' ') : [btn,btnWhite].join(' ')}>
       {text}
-    </NavLink>
+    </Link>
   )
 }
 
